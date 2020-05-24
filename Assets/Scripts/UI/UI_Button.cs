0cb021a724f6aff8cc4f9fb872ef56a86a6d7c71
Duarte Melo <duartemelo4@gmail.com>
2020-05-24 12:16:06 +0100

    star0.2

---
+++ Assets/Scripts/UI/UI_Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Button : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (button1 && button2)
+        {
+            button1.SetActive(true);
+            button2.SetActive(false);
+        }
+    }
+
+    public void SwitchButton()
+    {
+        if (button1.activeSelf)
+        {
+            button1.SetActive(false);
+            button2.SetActive(true);
+        }
+        else
+        {
+            button1.SetActive(true);
+            button2.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UI_Panel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Panel : MonoBehaviour
+{
+    public GameObject actionPanel;
+    public GameObject addCommentPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (actionPanel && addCommentPanel)
+        {
+            actionPanel.SetActive(true);
+            addCommentPanel.SetActive(false);
+        }
+    }
+
+    public void SwitchPanel()
+    {
+        if (actionPanel.activeSelf)
+        {
+            actionPanel.SetActive(false);
+            addCommentPanel.SetActive(true);
+        }
+        else
+        {
+            actionPanel.SetActive(true);
+            addCommentPanel.SetActive(false);
+        }
+    }
+}
