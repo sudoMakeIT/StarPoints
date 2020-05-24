e8b8ac3df75e62cfd4ecd331a2d6306c2572e835
Duarte Melo <duartemelo4@gmail.com>
2020-05-24 13:31:07 +0100

    star0.3

---
+++ Assets/Scripts/UI/UI_Text.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Text : MonoBehaviour
+{
+    public void ChangeText(Text aText, string str)
+    {
+        aText.text = str;
+    }
+
+}
