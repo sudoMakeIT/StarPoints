a2f8d019e2695bcc207da03129c0617eb495b6f9
Duarte Melo <duartemelo4@gmail.com>
2020-05-25 14:48:35 +0100

    Main

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeString(string aScene)
+    {
+        SceneManager.LoadScene(aScene);
+    }
+
+    public void ChangeInt(int aScene)
+    {
+        SceneManager.LoadScene(aScene);
+    }
+}
